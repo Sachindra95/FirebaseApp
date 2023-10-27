1667bd3d694d4514c11bf57264c74f866ea745f7
Sachindra Kumar Viswakarma <vsachindra@gmail.com>
2023-10-27 16:03:26 +0530

    fix sum issue

--- app/src/main/java/com/example/firebaseapp/MainActivity.kt
+++ app/src/main/java/com/example/firebaseapp/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Sachin 123")
+                    Greeting("Sachin 1223")
                 }
             }
         }
